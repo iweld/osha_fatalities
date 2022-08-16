661179a73f74df62fe9a7d939a3ab9996f81d02d
Jaime M. Shaker <eyeweld@gmail.com>
2022-08-15 21:05:09 -0500

    Added temp table

--- sql_schema.sql
+++ sql_schema.sql
@@ -35,26 +35,47 @@ incident_date|city        |state|description                                    
    2022-03-28|Cicero      |IL   |Elias Avila-Romero (37) fatally crushed under forklift.                     |Federal|Yes     |
    2022-03-28|Houston     |TX   |Margarito Ladezma (50) electrocuted by power lines while trimming trees.    |Federal|No      |
    2022-03-25|Williamsburg|IA   |Robert Chittick (63) fatally engulfed in corn bin.                          |State  |Yes     |
+
+-- Create a new temp table with 'cleaned' and formatted data   
+ 
+DROP TABLE IF EXISTS fatalities_cleaned
+CREATE TEMP TABLE fatalities_cleaned AS (
+	SELECT
+		incident_date,
+		to_char(incident_date, 'Day') AS day_of_week,
+		lower(city) AS city,
+		lower(state) AS state,
+		CASE
+			WHEN description LIKE '%(__)%' THEN split_part(description, ')', 2)
+			ELSE description
+		END AS description,
+		CASE
+			WHEN lower(plan) = 'state plan' THEN 'state'
+			ELSE 'federal'
+		END AS plan,
+		lower(citation) AS citation
+	FROM
+	 	fatalities
+);
+ 	
+SELECT * FROM fatalities_cleaned LIMIT 10;
+ 	
+-- Results:
+
+incident_date|day_of_week|city        |state|description                                               |plan   |citation|
+-------------+-----------+------------+-----+----------------------------------------------------------+-------+--------+
+   2022-04-22|Friday     |smyrna      |tn   | fatally struck by concrete pillar.                       |federal|no      |
+   2022-04-15|Friday     |provo       |ut   | died in fall from roof.                                  |federal|no      |
+   2022-04-14|Thursday   |toledo      |oh   | died after becoming caught in machine drive shaft.       |federal|yes     |
+   2022-04-10|Sunday     |mason city  |ia   | suffered fatal injuries in fall on sidewalk.             |federal|no      |
+   2022-04-08|Friday     |paton       |ia   | fatally crushed under excavator.                         |federal|yes     |
+   2022-04-07|Thursday   |spring      |tx   | died in fall from roof.                                  |federal|yes     |
+   2022-03-31|Thursday   |santa rita  |gu   | died in fall from ladder after contacting energized wire.|federal|yes     |
+   2022-03-28|Monday     |cicero      |il   | fatally crushed under forklift.                          |federal|yes     |
+   2022-03-28|Monday     |houston     |tx   | electrocuted by power lines while trimming trees.        |federal|no      |
+   2022-03-25|Friday     |williamsburg|ia   | fatally engulfed in corn bin.                            |federal|yes     |
    
-SELECT
-	incident_date,
-	to_char(incident_date, 'Day') AS day_of_week,
-	lower(city) AS city,
-	lower(state) AS state,
-	CASE
-		WHEN description LIKE '%(__)%' THEN split_part(description, ')', 2)
-		ELSE description
-	END AS description,
-	lower(plan) AS plan,
-	lower(citation) AS citation
-FROM
- 	fatalities
-ORDER BY 
- 	incident_date
- 	
- 	
- 	
- 	
+
  	
  	
  	
